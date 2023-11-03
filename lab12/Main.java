722bbae5160201da10ba053d1c9c8b44e3ab2b6d
Pixako <sorawis2546@gmail.com>
2023-11-04 03:04:57 +0900

    ,

--- lab12/Main.java
+++ lab12/Main.java
@@ -17,10 +17,28 @@ public class Main
 		game.generateNextMove();
 	}
 
+	static void demo3()
+	{
+		Puzzle8_651544 game = new Puzzle8_651544(new int [] {9,0,0,1,0,1,3,0,2,4,1,0,2,1,1,5,1,2,7,2,0,8,2,1,6,2,2});
+		game.nextMoveWithStack();
+		System.out.println(game.explored.size());
+		System.out.println("partial explored state");
+		for (Puzzle8State s: game.explored)
+		{
+			if (s.sequence[0] == 1 && s.sequence[1] == 2 && s.sequence[2] == 3 && s.sequence[3] == 4)
+			{
+				System.out.println(Arrays.toString(s.sequence));
+				break;
+			}
+		}
+
+	}
+
 	public static void main(String[] args)
 	{
 		demo1();
 		demo2();
+		demo3();
 	}
 
 	
--- lab12/Puzzle8_651544.java
+++ lab12/Puzzle8_651544.java
@@ -1,171 +1,182 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.*;
 
+class Puzzle8State {
+    private int score;
+    public int[] sequence;
 
-class Puzzle8State{
-	int[] sequence;
-	//int score;
-	public boolean equals(Object obj) {
-		if (obj == null) return false;
-		if (obj == this) return true;
-		if (!(obj instanceof Puzzle8State)) return false;
-		Puzzle8State o = (Puzzle8State) obj;
-		return Arrays.equals(this.sequence, o.sequence);
-	}
-}
+    public Puzzle8State(int[] sequence) {
+        this.score = 0;
+        this.sequence = sequence;
+    }
 
+    public void displayBoard() {
 
-public class Puzzle8_651544 {
-	int size = 3;
-	int[] sequence = new int[9];
-	ArrayList<Puzzle8State> explored;
-	Stack<Puzzle8State> dfs;
-	//HashMap<Puzzle8State, Puzzle8State> tileMap;
-
-
-
-	//Puzzle8_651544 game = new Puzzle8_651544(new int [] {9,0,0,1,0,1,3,0,2,4,1,0,2,1,1,5,1,2,7,2,0,8,2,1,6,2,2});
-    public Puzzle8_651544(int[] input) {
-        //this.sequence = sequence;
-        explored = new ArrayList<>();
-        dfs = new Stack<>();
-        dfs.push(new Puzzle8State());
-        dfs.peek().sequence = sequence;
-        //tileMap = new HashMap<>();
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<input.length; i+=3){
-            int value = input[i];
-            int position = input[i+1]*size + input[i+2];
-            map.put(position,value);
-        }
-        for(int i=0; i<sequence.length; i++){
-            sequence[i] = map.get(i);
+        for (int i = 0; i < sequence.length; i++) {
+            System.out.print(sequence[i]);
+            if (i % 3 == 2) {
+                System.out.println();
+            } else {
+                System.out.print(" ");
+            }
         }
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Puzzle8State)) return false;
+        Puzzle8State otherState = (Puzzle8State) obj;
+        return Arrays.equals(sequence, otherState.sequence);
+    }
 
-	boolean isGoal(Puzzle8State state)
-	{
-		int[] goal = {1,2,3,4,5,6,7,8,9};
-		return Arrays.equals(state.sequence, goal);
-	}
+    @Override
+    public String toString() {
+        return Arrays.toString(sequence);
+    }
 
-	static ArrayList<Puzzle8State> getNextStates(Puzzle8State currentState) {
-		ArrayList<Puzzle8State> nextStates = new ArrayList<>();
-		int[] sequence = currentState.sequence;
-		int size = (int) Math.sqrt(sequence.length);
-		int zeroIndex = 0;
-		for (int i = 0; i < sequence.length; i++) {
-			if (sequence[i] == 0) {
-				zeroIndex = i;
-				break;
-			}
-		}
-		int zeroRow = zeroIndex / size;
-		int zeroCol = zeroIndex % size;
-		if (zeroRow > 0) {
-			int[] newSequence = sequence.clone();
-			newSequence[zeroIndex] = newSequence[zeroIndex - size];
-			newSequence[zeroIndex - size] = 9;
-			Puzzle8State newState = new Puzzle8State();
-			newState.sequence = newSequence;
-			nextStates.add(newState);
-		}
-		if (zeroRow < size - 1) {
-			int[] newSequence = sequence.clone();
-			newSequence[zeroIndex] = newSequence[zeroIndex + size];
-			newSequence[zeroIndex + size] = 9;
-			Puzzle8State newState = new Puzzle8State();
-			newState.sequence = newSequence;
-			nextStates.add(newState);
-		}
-		if (zeroCol > 0) {
-			int[] newSequence = sequence.clone();
-			newSequence[zeroIndex] = newSequence[zeroIndex - 1];
-			newSequence[zeroIndex - 1] = 9;
-			Puzzle8State newState = new Puzzle8State();
-			newState.sequence = newSequence;
-			nextStates.add(newState);
-		}
-		if (zeroCol < size - 1) {
-			int[] newSequence = sequence.clone();
-			newSequence[zeroIndex] = newSequence[zeroIndex + 1];
-			newSequence[zeroIndex + 1] = 9;
-			Puzzle8State newState = new Puzzle8State();
-			newState.sequence = newSequence;
-			nextStates.add(newState);
-		}
-
-		return nextStates;
-	}
-	void nextMoveWithStack()
-	{
-		Puzzle8State currentState = dfs.pop();
-		if (isGoal(currentState))
-		{
-			System.out.println("Goal state found!");
-			return;
-		}
-		explored.add(currentState);
-		ArrayList<Puzzle8State> nextStates = getNextStates(currentState);
-		for (Puzzle8State state : nextStates)
-		{
-			if (!explored.contains(state))
-			{
-				dfs.push(state);
-			}
-		}
-		nextMoveWithStack();
-	}
-	
-		void displayBoard() {
-			for (int i = 0; i < sequence.length; i++) {
-				if (i % size == 0 && i != 0)
-				{
-					System.out.println();
-				}
-				if (sequence[i] != 9)
-				{
-					System.out.printf("%4d", sequence[i]);
-				}
-				else
-				{
-					System.out.printf("%4s", " ");
-				}
-			}
-			System.out.println();
-		}
-
-		void generateNextMove() {
-			Puzzle8State currentState = dfs.peek();
-			ArrayList<Puzzle8State> nextStates = getNextStates(currentState);
-			for (Puzzle8State state : nextStates) {
-				if (!explored.contains(state)) {
-					int zeroPosCurrent = getZeroPos(currentState.sequence);
-					int zeroPosNext = getZeroPos(state.sequence);
-					String direction = "";
-					if (zeroPosCurrent - zeroPosNext == size) {
-						direction = "pushing south";
-					} else if (zeroPosCurrent - zeroPosNext == -1) {
-						direction = "pushing east";
-					}
-					System.out.println(direction + " " + currentState.sequence[zeroPosNext] + " " + Arrays.toString(state.sequence));
-				}
-			}
-		}
-		
-		
-		int getZeroPos(int[] sequence) {
-			for (int i = 0; i < sequence.length; i++) {
-				if (sequence[i] == 9) {
-					return i;
-				}
-			}
-			return -1;
-		}
-
+    public int[] getSequence() {
+        return Arrays.copyOf(sequence, sequence.length);
+    }
 }
 
+class Puzzle8_651544 {
+    private int size = 3;
+    private int[] sequence = new int[size * size];
+    public ArrayList<Puzzle8State> explored = new ArrayList<>();
+    private Stack<Puzzle8State> dfs = new Stack<>();
+    public Puzzle8_651544(int[] sequence) {
+        this.sequence = oneDBoard(sequence);
+    }
+	public int[] swap(int[] sequence, int number1, int number2) {
+		int[] newSequence = sequence.clone();
+		int number1Index = 0;
+		int number2Index = 0;
+		for (int i = 0; i < sequence.length; i++) {
+			if (sequence[i] == number1) {
+				number1Index = i;
+			}
+			if (sequence[i] == number2) {
+				number2Index = i;
+			}
+		}
+		newSequence[number1Index] = number2;
+		newSequence[number2Index] = number1;
+		return newSequence;
+	}
+	
+	private int findBlankIndex() {
+		for (int i = 0; i < sequence.length; i++) {
+			if (sequence[i] == 9) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private void tryMove(int targetIndex, String direction) {
+		if(targetIndex >= 0 && targetIndex < sequence.length) {
+			int targetNumber = sequence[targetIndex];
+			int[] newSequence = swap(sequence, 9, targetNumber);
+			Puzzle8State newState = new Puzzle8State(newSequence);
+			if (!explored.contains(newState)) {
+				System.out.println("Pushing " + direction + " " + targetNumber + " " + Arrays.toString(newSequence));
+				dfs.push(newState);
+			}
+		}
+	}
+	
+	private void tryMoveDown() {
+		int blankIndex = findBlankIndex();
+		tryMove(blankIndex + 3, "South");
+	}
+	
+	private void tryMoveUp(){
+		int blankIndex = findBlankIndex();
+		tryMove(blankIndex - 3, "North");
+	}
+	
+	private void tryMoveEast(){
+		int blankIndex = findBlankIndex();
+		if(blankIndex % 3 < 2) {
+			tryMove(blankIndex + 1, "East");
+		}
+	}
+	
+	private void tryMoveWest(){
+		int blankIndex = findBlankIndex();
+		if(blankIndex % 3 > 0) {
+			tryMove(blankIndex - 1, "West");
+		}
+	}
+	
+    public void generateNextMove() {
+        tryMoveEast();
+        tryMoveWest();
+        tryMoveUp();
+        tryMoveDown();
+
+    }
+
+    public void displayBoard() {
+        for (int i = 0; i < sequence.length; i++)
+		{
+			
+            if (sequence[i] == 9) {
+                System.out.print("  ");
+            } else {
+                System.out.print(sequence[i] + " ");
+            }
+            if ((i + 1) % 3 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
+
+    public int[] oneDBoard(int[] sequence) {
+        int[] board = new int[size * size];
+        for (int i = 0; i < sequence.length; i=i+3) {
+            int number = sequence[i];
+            int row = sequence[i+1];
+            int column = sequence[i+2];
+            board[row * size + column] = number;
+        }
+        return board;
+    }
+
+
+
+
+    public void nextMoveWithStack() {
+        generateNextMove();
+        if (dfs.isEmpty()) {
+            System.out.println("No more moves");
+            return;
+        }
+
+        while (!dfs.isEmpty()) {
+            Puzzle8State state = dfs.pop();
+            sequence = state.sequence;
+            if (isGoal(sequence)) {
+                System.out.println("Goal reached");
+                return;
+            }
+            explored.add(state);
+            generateNextMove();
+            displayBoard();
+        }
+
+
+    }
+
+    public boolean isGoal(int[] seq) {
+        int[] goal = new int[] {1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i < seq.length; i++) {
+            if(seq[i] != goal[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
